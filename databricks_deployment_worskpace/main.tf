f229ce4842f18ff26fc015393e52b8c9caeb6b25
LukaszSawaData <83174550+LukaszSawaData@users.noreply.github.com>
2025-10-06 23:18:12 +0200

    Update main.tf

--- databricks_deployment_worskpace/main.tf
+++ databricks_deployment_worskpace/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "first_vnet"
-  address_space       = "10.0.0.0/24"
+  address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -37,7 +37,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     ="10.0.0.0/24"
+  address_prefixes     =["10.0.0.0/24"]
 }
 
 resource "azurerm_databricks_workspace" "dbw" {
